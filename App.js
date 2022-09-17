4bf7f8c4c277d8fd380ae808e6ff9aad56981746
TabogaBr <brenda.taboga7@gmail.com>
2022-09-17 15:24:46 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Alert, Image } from 'react-native';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [meals, setMeals] = useState([]);
+
+  const getMeals = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then(response => response.json())
+      .then(data => setMeals(data.meals))
+      .catch(error => {
+        Alert.alert('Error', error);
+      });
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        style={styles.marginTop}
+        renderItem={({ item }) =>
+          <View>
+            <Text style={styles.text}>{item.strMeal}</Text>
+            <Image
+              style={styles.image}
+              source={{ uri: item.strMealThumb }}
+            />
+          </View>}
+        ItemSeparatorComponent={listSeparator}
+        data={meals} />
+      <TextInput
+        style={styles.inputs}
+        placeholder='keyword'
+        onChangeText={text => setKeyword(text)} />
+      <Button title="FIND" onPress={getMeals} />
     </View>
   );
 }
@@ -16,5 +57,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputs: {
+    fontSize: 18,
+    width: 200,
+  },
+  text: {
+    fontSize: 16,
+  },
+  marginTop: {
+    marginTop: 50,
+    width: 280,
+  },
+  image: {
+    width: 60,
+    height: 60,
   },
 });
